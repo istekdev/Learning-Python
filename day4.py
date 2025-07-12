f6ec7b91f876be265ad25899505e16d24395bef7
ISTEK <tamdacoder@gmail.com>
2025-07-12 18:41:06 -0500

    Update day4.py

--- day4.py
+++ day4.py
@@ -11,13 +11,13 @@ finalentropy = input("TYPE IN A PHRASE: ")
 def doentropy():
     entropyfirst = 0 # Somehow ensures that the for loop uses all characters, without this, it would only use the very last character
     for words in firstpass: # Loops though firstpass, for means selecting something and in means where it's selected
-        entropyfirst = ord(words) # It converts the characters to ASCII code
+        entropyfirst += ord(words) # It converts the characters to ASCII code
     entropysec = 0
     for characters in finalentropy:
-        entropysec = ord(characters)
+        entropysec += ord(characters) # Suppsedly, += means including all the ASCII code of each letter
     finality = (entropyfirst + entropysec) * gettime
     output = print(round(finality)) # Using round, I don't get an endless decimal
 
 doentropy()
 
-# This utilizes known concepts (def, print, time, import, input, and int), and uses new concepts like (for, in, ord, and round)
+# This utilizes known concepts (def, print, time, import, input, and int), and uses new concepts like (for, in, ord, +=, and round)
