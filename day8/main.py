41a9c6a7e425c55d3042d5d091c3966191d0d186
ISTEK <tamdacoder@gmail.com>
2025-07-18 23:00:57 -0500

    Update main.py

--- day8/main.py
+++ day8/main.py
@@ -1,6 +1,21 @@
+# Day 8 of Python
+# This is by far the most mentally draining things I made. And worst of all, this is just a warmup.
+# I planned to learn how to locally import Python files using import, and interact with txt files using with. 
+# But instead, I got bombareded with new concepts. It's like giving a Pre-Algebra student College Calculus when they got a 100% on their test.
+# Hell, I don't even understand some of them. Like look at these:
+# zfill, [i:i+3], % 3, //, return, and global
+# But some were also unexpected yet easier to learn, like eval, which acted as a non-linear equation calculator. And the function "or", which, as it sounds, gives an alternative. 
+# ---
+# Aparrently, zfill() "pads" a string, basically adds zeroes behind a number (eg: 5 -> 005)
+# For [i:i+3], it looks Calculus-like, but it just splits strings. So if I were to have a variable equaling to "abcde," and I did [0:3], i'd get "abc." The +3 means that I only get a chunk of 3.
+# % 3 is more complex, % returns the remainder of an equation (eg: 7 % 3 = 1, basically 7/3 is 2, the remainder is 1.. we on the same page..?)
+# // is floor divison, removes the decimal. No rounding.
+# return.. well.. returns a value or function
+# global makes a variable globally usable.
+
+
 import encrypt
 import time
-import os
 
 def granted():
   print("ACCESS GRANTED - WELCOME TO MY VAULT")
@@ -30,10 +45,9 @@ def write():
   writeaction = input("CHOOSE ACTION (SAVE/LEAVE): ")
   if writeaction.upper() == "SAVE":
     with open("vault.txt", "a") as writing:
-        writing.write(str(writein))
+        encrypt.encryptsaves(writein)
         print("CONTENT SAVED")
         time.sleep(1)
-        encrypt.encryptsaves(writein)
         granted()
   elif writeaction.upper() == "LEAVE":
     granted()
@@ -43,10 +57,9 @@ def new():
   newaction = input("CHOOSE ACTION (SAVE/LEAVE): ")
   if newaction.upper() == "SAVE":
     with open("vault.txt", "w") as overwrote:
-        overwrote.write(str(newin))
+        encrypt.encryptsaves(newin)
         print("CONTENT SAVED")
         time.sleep(1)
-        encrypt.encryptsaves(newin)
         granted()
   elif newaction.upper() == "LEAVE":
     granted()
@@ -84,5 +97,5 @@ with open("pass.txt", "r") as password:
 
 # This uses concepts from days 1-7: def, if, else, elif, print, input, import (external), random, time, not, and str
 # This uses new concepts like upper, with, and import (locally)
-# Could not use for, ord, int, while, and random since encrypt.py uses it
-# Could not use eval since I don't know what do use it for (no non-linear math here) class, try, except, and range
+# Could not use for, ord, int, range, while, and random since encrypt.py uses it
+# Could not use eval since I don't know what do use it for (no non-linear math here) class, try, and except
