076a9156927a3c1017eed5541288d6ca4ceb8991
ISTEK <tamdacoder@gmail.com>
2025-07-20 22:36:03 -0500

    Create main.py

---
+++ day10/main.py
@@ -0,0 +1,24 @@
+# Blueprint:
+# This is the brains of the OS, responsible for the utilization of external and local modules.
+# For simplicity, this will be the one to run all local files together as one.
+# ---
+# Also, I took a day-long break, so technically, this is day 10.
+
+import time
+import encryption
+import system
+import confirmation
+
+print("LOADING ASSETS...")
+time.sleep(1)
+confirmation.encryption()
+time.sleep(1)
+confirmation.system()
+time.sleep(1)
+confirmation.txt()
+time.sleep(1)
+confirmation.verification()
+time.sleep(1)
+print("WELCOME TO PYOS 10")
+time.sleep(1)
+system.lock()
