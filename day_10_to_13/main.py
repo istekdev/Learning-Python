09edfbc39497a5a9116d95b74f05e74aad2456d5
ISTEK <istekdev@proton.me>
2025-09-30 08:51:02 -0500

    Create main.py

---
+++ day_10_to_13/main.py
@@ -0,0 +1,12 @@
+import time
+import gambling
+import existence
+
+print("WELCOME TO GAMBLEPY")
+time.sleep(1)
+existence.verifyfiles()
+time.sleep(1)
+existence.verifybalance()
+time.sleep(1)
+gambling.options()
+
